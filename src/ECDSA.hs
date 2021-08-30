c0577e4aad2e17fa1b856685b06dc041348214d0
harutotoro <haruto.y0327@gmail.com>
2021-08-30 10:18:23 +0000

    add hash lib

--- src/ECDSA.hs
+++ src/ECDSA.hs
@@ -1,10 +1,11 @@
-import Data.Bits
+--import Data.Bits
 import Data.Digest.Pure.SHA
+--import qualified Data.ByteString as B
 
 a=0;  -- parameters Y^2=X^3+aX+b and 4a^3+27b^2!=0
 b=7;  --   Y^2=X^3+7 
 p=223 -- Finite Field Parameter
-G=(15,18)   --  Base point
+--G=(15,18)   --  Base point
 n=17
 h=2
 ----------------------------------------------------------------
@@ -17,7 +18,7 @@ h=2
 -- chose random k between [1,n-1]
 -- cluc kG (x1,x2)and r=x1 mod n 
 
--- e=sha256(m)
+-- e=sha256(m) hash function 
 -- s= k^(-1)(e+dr) mod n -> sig(r,s)
 ----------------------------------------------------------------
 -- Verify 
